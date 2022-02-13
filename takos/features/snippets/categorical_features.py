ac7a1d00c0d87e26f8083aca6bdabbadab3e6bb3
TatsuyaSSK <ssktty514@gmail.com>
2022-02-13 17:00:20 +0900

    one-hot追加

--- takos/features/snippets/categorical_features.py
+++ takos/features/snippets/categorical_features.py
@@ -1,24 +1,52 @@
-import pandas as pd
 from typing import *
 
+import pandas as pd
+
+
 # count encoding
-def create_count_encoding_feature(input_df: pd.DataFrame, use_columns: List[str]) -> pd.DataFrame:
+def create_count_encoding(input_df: pd.DataFrame, use_columns: List[str]) -> pd.DataFrame:
     """
     カウントエンコーディングを行う関数
 
     Args:
         input_df (pd.DataFrame): インプットとなるデータフレーム
-        use_columns (List[str]): カウントエンコーディングを行う対象となるカラム
+        use_columns (List[str]): 対象となるカラム
 
     Returns:
-        pd.DataFrame: カウントエンコーディング後のデータフレーム
+        pd.DataFrame: 適用後のデータフレーム
     """
     out_df = pd.DataFrame()
 
     for column in use_columns:
         # 各カラムの値ごとにinput_dfにおけるカウント数を集計する
         value_counts = input_df[column].value_counts()
-        out_df[column] = input_df[column].map(vc)
+        out_df[column] = input_df[column].map(value_counts)
 
     return out_df.add_prefix("CE_")
 
+
+def create_one_hot_encoding(input_df: pd.DataFrame, use_columns: List[str]) -> pd.DataFrame:
+    """
+    ワンホットエンコーディングを行う関数
+
+    Args:
+        input_df (pd.DataFrame): インプットとなるデータフレーム
+        use_columns (List[str]): 対象となるカラム
+
+    Returns:
+        pd.DataFrame: 適用後のデータフレーム
+    """
+    out_df = pd.DataFrame()
+
+    for column in use_columns:
+        value_counts = input_df[column].value_counts()
+
+        cat = pd.Categorical(input_df[column], categories=value_counts.index)
+
+        # このタイミングでone-hot化
+        out_i = pd.get_dummies(cat)
+        out_i.columns = out_i.columns.tolist()
+        out_i = out_i.add_prefix(f"{column}=")
+        out_df = pd.concat([out_df, out_i], axis=1)
+
+    return out_df
